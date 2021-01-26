7cc113d3081e357e990fd977e6b0702e5352e6ef
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-01-26 15:39:14 +0200

    hide-complete

--- event delegation/tree menu/script.js
+++ event delegation/tree menu/script.js
@@ -2,8 +2,10 @@ const tree=document.getElementById('tree');
 tree.addEventListener('click', showHide);
 
 function showHide(event){
-    if(event.target.tagName!=='LI')
-    return ;
-    let li=document.getRootNode()
-    li.hidden=!li.hidden;
+    if(event.target.tagName!=='SPAN'){
+        return;
+    }
+    let hid=event.target.parentNode;
+    let hid2= hid.childNodes[2];
+    hid2.hidden=!hid2.hidden;
 }
